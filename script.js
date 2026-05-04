8c6043e9c6dba424fe7ea4df465bdfa622b42953
CottonnottoC <archwathpailee@gmail.com>
2026-05-04 18:26:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ทดสอบให้ปุ่มขึ้นตอน 5 โมง 5 นาทีของวันนี้
 // เปิดให้เข้าตอนวันที่ 5/5 เวลา 18:30 ตามเวลาไทย
-const TARGET_DATE = new Date('2026-05-05T18:30:00+07:00');
+const TARGET_DATE = new Date('2026-05-04T18:30:00+07:00');
 
 const TEST_MODE = false;
 const imageMap = {
